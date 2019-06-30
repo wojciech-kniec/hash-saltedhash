160a7384bf573896fcc27a9b495a235f943a1c69
wojciech-kniec <kniecwojciech@gmail.com>
2019-06-30 23:42:42 +0200

    Encrypt Decrypt class added

--- src/main/java/EncryptDecrypt.java
+++ src/main/java/EncryptDecrypt.java
@@ -4,16 +4,25 @@ public class EncryptDecrypt {
         for (int i = 0; i < pass.length(); i++) {
             temp = pass.charAt(i);
             temp *= 2;
-            System.out.println(temp);
+            System.out.print(temp);
         }
 
     }
-    public static void decrypt(String pass){
+
+    public static void decrypt(String pass) {
         char temp;
         for (int i = 0; i < pass.length(); i++) {
             temp = pass.charAt(i);
-            t
-
+            temp /= 2;
+            System.out.print(temp);
         }
+
+    }
+
+    public static void main(String[] args) {
+        encrypt("SomeSuperStrong Password");
+
+        System.out.println();
+        decrypt("¦ÞÚÊ¦êàÊä¦èäÞÜÎ@ ÂææîÞäÈ");
     }
 }
